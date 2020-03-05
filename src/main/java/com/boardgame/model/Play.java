ff4bf9d0d3c50e8e7943c037312ab32a148c19fe
justine <justine.gracia@gmail.com>
2020-03-05 11:46:03 +0100

    addition foreign keys  in Play class

--- src/main/java/com/boardgame/model/Play.java
+++ src/main/java/com/boardgame/model/Play.java
@@ -1,56 +1,77 @@
 package com.boardgame.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
 @Table(name = "partie")
 public class Play {
 
-        @Id
-        private Integer id;
+    @Id
+    private Integer id;
 
-        @Column(name = "CREATION_DATE")
-        private Date creationDate;
+    @Column(name = "CREATION_DATE")
+    private Date creationDate;
 
-        @Column(name = "GAME_DATE")
-        private Date gameDate;
+    @Column(name = "GAME_DATE")
+    private Date gameDate;
 
-        @Column(name = "SCORE")
-        private int score;
+    @Column(name = "SCORE")
+    private int score;
 
-        public Integer getId() {
-            return id;
-        }
+    @ManyToOne(targetEntity = Player.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "WINNER_ID", referencedColumnName = "ID")
+    private Player player;
 
-        public void setId(Integer id) {
-            this.id = id;
-        }
+    @ManyToOne(targetEntity = Game.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "GAME_ID", referencedColumnName = "ID")
+    private Game game;
 
-        public Date getCreationDate() {
-            return creationDate;
-        }
-
-        public void setCreationDate(Date creationDate) {
-            this.creationDate = creationDate;
-        }
-
-        public Date getGameDate() {
-            return gameDate;
-        }
-
-        public void setGameDate(Date gameDate) {
-            this.gameDate = gameDate;
-        }
-
-        public int getScore() {
-            return score;
-        }
-
-        public void setScore(int score) {
-            this.score = score;
-        }
+    public Game getGame() {
+        return game;
     }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getGameDate() {
+        return gameDate;
+    }
+
+    public void setGameDate(Date gameDate) {
+        this.gameDate = gameDate;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
