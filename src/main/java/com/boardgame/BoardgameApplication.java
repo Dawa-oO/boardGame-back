d31b9ed97252a5c237d28f24eb594d7b9f243c5d
Gautier Lemasson <gautier.lemasson@intech.lu>
2020-03-19 18:01:59 +0100

    Enable gettin static external picture

--- src/main/java/com/boardgame/BoardgameApplication.java
+++ src/main/java/com/boardgame/BoardgameApplication.java
@@ -1,10 +1,12 @@
 package com.boardgame;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
@@ -13,14 +15,17 @@ import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration
 @SpringBootApplication
 public class BoardgameApplication extends SpringBootServletInitializer implements WebMvcConfigurer {
 
+	@Autowired
+	private Environment env;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BoardgameApplication.class, args);
 	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**")
-				.addResourceLocations("classpath:/static/");
+		System.out.println("uploadDirectory=" + env.getProperty("picture.path"));
+		registry.addResourceHandler("/**").addResourceLocations("file:" + env.getProperty("picture.path"));
 	}
 
 	@Override
