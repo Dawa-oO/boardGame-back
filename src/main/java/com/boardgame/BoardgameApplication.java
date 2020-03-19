ce715737b6bd283e2fb0271ee9c033386a936e84
Gautier Lemasson <gautier.lemasson@intech.lu>
2020-03-19 16:36:26 +0100

    Enable getting static picture

--- src/main/java/com/boardgame/BoardgameApplication.java
+++ src/main/java/com/boardgame/BoardgameApplication.java
@@ -5,14 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 
 @Import({ BeanValidatorPluginsConfiguration.class})
 @SpringBootApplication
-public class BoardgameApplication extends SpringBootServletInitializer {
+public class BoardgameApplication extends SpringBootServletInitializer implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardgameApplication.class, args);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**")
+				.addResourceLocations("classpath:/static/");
 	}
 
 	@Override
