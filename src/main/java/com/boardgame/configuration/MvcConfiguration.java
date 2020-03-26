e32638b70ca2bf5ac704df68e7a023155652bbc0
Gautier Lemasson <gautier.lemasson@intech.lu>
2020-03-26 19:18:02 +0100

    Fix CORS policy

--- src/main/java/com/boardgame/configuration/MvcConfiguration.java
+++ src/main/java/com/boardgame/configuration/MvcConfiguration.java
@@ -3,6 +3,7 @@ package com.boardgame.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,6 +17,11 @@ public class MvcConfiguration implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("file:" + env.getProperty("picture.path"));
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 
 }
